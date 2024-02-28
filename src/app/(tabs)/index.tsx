9a22c60ead50b5030967ea6898a25241ab391c2a
MonetCarriere <MonetCarriere@yahoo.com>
2024-02-28 08:43:34 -0800

    fifth commit

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,19 +4,30 @@ import products from '../../../assets/data/products'
 
 const product = products[0]
 
-export default function TabOneScreen() {
+const ProductListItem = () => {
   return (
-    <View style={styles.container}>
+     <View style={styles.container}>
       <Image source={{uri: product.image}} style={styles.image}/>
       <Text style={styles.title}>{product.name}</Text>
       <Text>{product.price}</Text>
+    </View>
+  )
+}
+
+
+export default function MenuScreen() {
+  return (
+    <View>
+     <ProductListItem/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
   },
   image:{
     width: '100%',
