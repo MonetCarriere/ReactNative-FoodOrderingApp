57eca471b2067b3f582144c7616cc1f30475d02f
MonetCarriere <MonetCarriere@yahoo.com>
2024-02-28 06:34:01 -0800

    second commit

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,21 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import Colors from '../../constants/Colors'
+import products from '../../../assets/data/products'
 
+const product = products[0]
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pizza Peperoni</Text>
-      <Text>$12.99</Text>
+      <Image source={{uri: product.image}} style={styles.image}/>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text>{product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    
+  },
+  image:{
+    width: 100,
+    height: 100,
   },
     title: {
     fontSize: 18,
