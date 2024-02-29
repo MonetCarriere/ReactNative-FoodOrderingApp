8b5a0cd65bc8b261a962911603310a74e17c9541
MonetCarriere <MonetCarriere@yahoo.com>
2024-02-29 06:31:19 -0800

    seventh commit

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,18 +1,7 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '../../constants/Colors'
+import {View} from 'react-native';
 import products from '../../../assets/data/products'
+import ProductListItem from '@/src/components/ProductListItem';
 
-
-
-const ProductListItem = ({product}) => {
-  return (
-     <View style={styles.container}>
-      <Image source={{uri: product.image}} style={styles.image}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text>{product.price}</Text>
-    </View>
-  )
-}
 
 
 export default function MenuScreen() {
@@ -24,22 +13,3 @@ export default function MenuScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image:{
-    width: '100%',
-    aspectRatio: 1,
-  },
-    title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-  }
-});
